3abad336383f8f9a370452332395fdc82844d2b2
Dominik Kremer <dominik.kremer@gmx.de>
2025-06-07 15:50:40 +0200

    Add linting

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'rec-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'recipe-browser';
